9bfca12a44d3ad1b898a4d2253c6425d5cb09530
Andreas Furster <andreas.furster@hotmail.com>
2018-12-02 15:36:52 +0100

    Basic project setup

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,11 +1,12 @@
 <?php
 
-use Phalcon\Mvc\Controller;
-
-class IndexController extends Controller
+class IndexController extends ControllerBase
 {
+
     public function indexAction()
     {
-        echo '<h1>Hello!</h1>';
+
     }
+
 }
+
--- public/index.php
+++ public/index.php
@@ -1,58 +1,47 @@
 <?php
-
-use Phalcon\Loader;
-use Phalcon\Mvc\View;
-use Phalcon\Mvc\Application;
 use Phalcon\Di\FactoryDefault;
-use Phalcon\Mvc\Url as UrlProvider;
 
-// Define path constants
+error_reporting(E_ALL);
+
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
 
-// Create an autoloader
-$loader = new Loader();
-
-// Load whole directories
-$loader->registerDirs(
-    [
-        APP_PATH . '/controllers/',
-        APP_PATH . '/models/',
-    ]
-);
-
-$loader->register();
-
-// Create a DI
-$di = new FactoryDefault();
-
-// Setup the view service
-$di->set(
-    'view',
-    function () {
-        $view = new View();
-        $view->setViewsDir(APP_PATH . '/views/');
-        return $view;
-    }
-);
-
-// Setup a base URI
-$di->set(
-    'url',
-    function () {
-        $url = new UrlProvider();
-        $url->setBaseUri('/');
-        return $url;
-    }
-);
-
-$application = new Application($di);
-
 try {
-    // Handle the request
-    $response = $application->handle();
 
-    $response->send();
+    /**
+     * The FactoryDefault Dependency Injector automatically registers
+     * the services that provide a full stack framework.
+     */
+    $di = new FactoryDefault();
+
+    /**
+     * Handle routes
+     */
+    include APP_PATH . '/config/router.php';
+
+    /**
+     * Read services
+     */
+    include APP_PATH . '/config/services.php';
+
+    /**
+     * Get config service for use in inline setup below
+     */
+    $config = $di->getConfig();
+
+    /**
+     * Include Autoloader
+     */
+    include APP_PATH . '/config/loader.php';
+
+    /**
+     * Handle the request
+     */
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo str_replace(["\n","\r","\t"], '', $application->handle()->getContent());
+
 } catch (\Exception $e) {
-    echo 'Exception: ', $e->getMessage();
+    echo $e->getMessage() . '<br>';
+    echo '<pre>' . $e->getTraceAsString() . '</pre>';
 }
